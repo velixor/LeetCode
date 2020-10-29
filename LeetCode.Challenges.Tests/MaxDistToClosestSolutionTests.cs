5db34b2d1c506c36033700143a7fef6df0a0dd27
Pavel Dushkov <velixor.work@gmail.com>
2020-10-29 15:27:16 +0200

    solve MaxDistToClosest

---
+++ LeetCode.Challenges.Tests/MaxDistToClosestSolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Challenges.Tests
+{
+    [TestFixture]
+    public class MaxDistToClosestSolutionTests
+    {
+        [Test]
+        [TestCase(new[] {1, 0, 0, 1}, 1)]
+        [TestCase(new[] {1, 0, 0, 0, 1, 0, 1}, 2)]
+        [TestCase(new[] {1, 0}, 1)]
+        [TestCase(new[] {1, 0, 0}, 2)]
+        public void MaxDistToClosest(int[] seats, int dist)
+        {
+            var solution = new MaxDistToClosestSolution();
+            Assert.That(solution.MaxDistToClosest(seats), Is.EqualTo(dist));
+        }
+    }
+}
---
+++ LeetCode.Challenges/MaxDistToClosestSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Challenges
+{
+    public class MaxDistToClosestSolution
+    {
+        // https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/563/week-5-october-29th-october-31st/3512/
+        public int MaxDistToClosest(int[] seats)
+        {
+            var left = -1;
+            var right = 0;
+            var maxDist = 0;
+
+            while (right < seats.Length)
+            {
+                if (seats[right] == 1)
+                {
+                    maxDist = left < 0
+                        ? Math.Max(maxDist, CornerCase(left, right))
+                        : Math.Max(maxDist, MiddleCase(left, right));
+                    left = right;
+                }
+
+                right++;
+            }
+
+            maxDist = Math.Max(maxDist, CornerCase(left, right));
+
+            return maxDist;
+        }
+
+        private int CornerCase(int l, int r) => r - l - 1;
+        private int MiddleCase(int l, int r) => (int) Math.Ceiling(CornerCase(l, r) / 2.0);
+    }
+}
